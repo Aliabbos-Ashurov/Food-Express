cece99897bb8b176228aa7b2289064652541688c
Aliabbos Ashurov <aliabbosashurov@gmail.com>
2024-05-06 09:19:02 +0500

    ...

--- src/main/java/com/pdp/frontend/Test.java
+++ src/main/java/com/pdp/frontend/Test.java
@@ -1,8 +1,5 @@
 package com.pdp.frontend;
 
-import com.pdp.java.console.support.Displayable;
-import com.pdp.json.serializer.JsonSerializer;
-
 /**
  * @author Aliabbos Ashurov
  * @since: 30/April/2024  21:09
