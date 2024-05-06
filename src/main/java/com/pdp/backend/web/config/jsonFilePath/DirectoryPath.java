6ccb95ee91620ebc2f9c3d81469cbe55a99b18a7
Aliabbos Ashurov <aliabbosashurov@gmail.com>
2024-05-06 10:08:00 +0500

    new interface DirectoryPath added for directory link.

---
+++ src/main/java/com/pdp/backend/web/config/jsonFilePath/DirectoryPath.java
@@ -0,0 +1,9 @@
+package com.pdp.backend.web.config.jsonFilePath;
+
+/**
+ * @author Aliabbos Ashurov
+ * @since 06/May/2024  09:33
+ **/
+public interface DirectoryPath {
+     String DIRECTORY = "db/";
+}
