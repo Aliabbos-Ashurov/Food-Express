c20cf56f6c50d865118ed6115c0bb5c57631eda2
Aliabbos Ashurov <aliabbosashurov@gmail.com>
2024-05-06 12:10:53 +0500

    interfaces were written to store image addresses

---
+++ src/main/java/com/pdp/backend/web/config/imagesPath/BrandImagesPath.java
@@ -0,0 +1,8 @@
+package com.pdp.backend.web.config.imagesPath;
+
+/**
+ * @author Aliabbos Ashurov
+ * @since 06/May/2024  09:28
+ **/
+public interface BrandImagesPath {
+}
---
+++ src/main/java/com/pdp/backend/web/config/imagesPath/FoodImagesPath.java
@@ -0,0 +1,8 @@
+package com.pdp.backend.web.config.imagesPath;
+
+/**
+ * @author Aliabbos Ashurov
+ * @since 06/May/2024  12:10
+ **/
+public interface FoodImagesPath {
+}
