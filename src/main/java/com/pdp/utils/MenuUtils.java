1e00933d70eea8c868c9a390e3139192b153eeab
Doniyor Nishonov <nishonovd80@gmail.com>
2024-05-11 13:46:44 +0500

    MenuUtils method and fields have been rewritten

--- src/main/java/com/pdp/utils/MenuUtils.java
+++ src/main/java/com/pdp/utils/MenuUtils.java
@@ -1,19 +1,32 @@
 package com.pdp.utils;
 
-import static com.pdp.java.console.Scan.scanInt;
+import com.pdp.utils.source.MessageSourceUtils;
+import com.pdp.web.enums.Language;
 
 /**
  * @author Doniyor Nishonov
  * Date: 10/May/2024  21:24
  **/
 public interface MenuUtils {
-    String MENU = """
-            ----Menu----
-            [1] Sign In
-            [2] Sign Up
-            [0] Exit
-            """;
-    static void menu(String str){
-        System.out.println(str);
+    //---USER MENU
+    String BACK = "main.menu.back";
+    String LANGUAGE = "main.menu.language";
+    String SIGN_IN_UP = "main.menu.signInUP";
+    String USER_MENU_AFTER_LOG = "menu.after.log.department";
+    String MAKE_ORDER = "menu.after.department.order";
+    String USER_ORDER = "menu.after.department.my.order";
+    String CART_OPERATION = "menu.after.department.order.cart";
+    String CONFIRMATION = "menu.confirmation";
+
+    // DELIVERER
+    String DELIVERER_MENU = "main.menu.deliver";
+    String DELIVERER_GENERAL_MENU = "menu.deliver.order.general";
+    String DELIVERER_GENERAL_FOLLOWING = "menu.deliver.order.general.following";
+
+    static void menu(String menuKey, Language language) {
+        String result = language.equals(Language.UZ)
+                ? MessageSourceUtils.getLocalizedMessage(menuKey, Language.UZ)
+                : MessageSourceUtils.getLocalizedMessage(menuKey, Language.EN);
+        System.out.println(result);
     }
 }
