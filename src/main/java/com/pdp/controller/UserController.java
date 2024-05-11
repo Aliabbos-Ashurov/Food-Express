459dcdd640a919154bb8b45bf1773671b898761f
Aliabbos Ashurov <aliabbosashurov@gmail.com>
2024-05-11 18:50:43 +0500

    ...

--- src/main/java/com/pdp/controller/UserController.java
+++ src/main/java/com/pdp/controller/UserController.java
@@ -4,27 +4,28 @@ import com.pdp.web.enums.role.Role;
 import com.pdp.web.model.user.User;
 
 /**
+ * Controller to manage user interactions within the application.
+ *
+ * Date: 11/May/2024 14:37
  * @author Doniyor Nishonov
- * Date: 11/May/2024  14:37
  **/
 public class UserController {
-    public static User curUser;
+    private static User currentUser;
 
-    public static void startApp() {
-        if (LoginController.userSignInSignUp())
-            afterLog();
-        else startApp();
+    public static void startApplication() {
+        boolean signedIn = LoginController.userSignInSignUp();
+        if (signedIn) {
+            handlePostLogin();
+        } else {
+            startApplication();
+        }
     }
 
-    private static void afterLog() {
-        if (curUser.getRole().equals(Role.USER)) {
-            while (true) {
-                CustomerOrderController.start();
-            }
-        } else if (curUser.getRole().equals(Role.DELIVERER)) {
-            while (true) {
-
-            }
+    private static void handlePostLogin() {
+        if (currentUser.getRole() == Role.USER) {
+            CustomerOrderController.start();
+        } else if (currentUser.getRole() == Role.DELIVERER) {
+            //...
         }
     }
 }
