65ef6e573c0aeaa7c97d9fbb14e514d1832a79f8
Aliabbos Ashurov <aliabbosashurov@gmail.com>
2024-05-11 15:45:25 +0500

    utils created for get element by index from list

---
+++ src/main/java/com/pdp/utils/Utils.java
@@ -0,0 +1,23 @@
+package com.pdp.utils;
+
+import java.util.List;
+
+/**
+ * @author Aliabbos Ashurov
+ * @since 11/May/2024  15:42
+ **/
+public class Utils {
+
+    /**
+     * Retrieves an element from a list based on the provided index.
+     *
+     * @param <T>    The type of elements in the list.
+     * @param list   The list from which to retrieve the element.
+     * @param choice The 1-based index of the element to retrieve.
+     * @return The element at the specified index or null if the index is out of range.
+     */
+    public static <T> T getElementByIndex(List<T> list, int choice) {
+        if (choice < 1 || choice > list.size()) return null;
+        return list.get(choice - 1);
+    }
+}
