b36cf2058f703ed9b9faf941507c2cfa751acf5a
Doniyor Nishonov <nishonovd80@gmail.com>
2024-05-11 15:47:32 +0500

    UserController class added to project

---
+++ src/main/java/com/pdp/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.pdp.controller;
+
+import com.pdp.web.enums.role.Role;
+import com.pdp.web.model.user.User;
+
+/**
+ * @author Doniyor Nishonov
+ * Date: 11/May/2024  14:37
+ **/
+public class UserController {
+    public static User curUser;
+
+    public static void startApp() {
+        if (LoginController.userSignInSignUp())
+            afterLog();
+        else startApp();
+    }
+
+    private static void afterLog() {
+
+    }
+}
